1fdccb43435b2b32985d936923a29f81a555630e
Oloer Cristian <mac.x.unity@gmail.com>
2022-03-22 12:55:13 +0200

    Edited method1 name, added a new parameter, edited method2 name, created file1.txt, created file2.txt

--- exNumbers.java
+++ exNumbers.java
@@ -1,14 +1,14 @@
 public class exNumbers {
     public static void main(String[] args) {
-        int num1 = 10, num2 = 10;
-        System.out.printf("Num1 = %d,  Num2 = %d\n\n", num1, num2);
+        int num1 = 10, num2 = 10, num3 = 20;
+        System.out.printf("Num1 = %d,  Num2 = %d,  Num3 = %d\n\n", num1, num2, num3);
 
-        addTwoNumbers(num1,num2);
+        addThreeNumbers(num1, num2, num3);
         substrTwoNumbers(num1,num2);
     }
 
-    public static void addTwoNumbers(int n1, int n2) {
-        System.out.println("SUM = " + (n1 + n2));
+    public static void addThreeNumbers(int n1, int n2, int n3) {
+        System.out.println("SUM = " + (n1 + n2 + n3));
     }
 
     public static void substrTwoNumbers(int a, int b) {
